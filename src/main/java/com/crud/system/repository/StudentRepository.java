6e28c9301a281e2989e873e87b5ca45d612c8131
Manuel-Luemba <mmatosop7@gmail.com>
2023-04-09 22:44:29 +0100

    Começando a construir a minha Interface StudentRepository, para poder Usar os metodos da Interface JpaRepository.

--- src/main/java/com/crud/system/repository/StudentRepository.java
+++ src/main/java/com/crud/system/repository/StudentRepository.java
@@ -1,4 +1,8 @@
 package com.crud.system.repository;
 
-public interface StudentRepository {
+import com.crud.system.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository  extends JpaRepository<Student,Long> {
+
 }
