cf473988ba322c4d96a1ca4d03345a40a5d41cde
Manuel-Luemba <mmatosop7@gmail.com>
2023-04-10 13:29:04 +0100

    Implementando a Relação de OneToMany entre as Categoria e SubCategoria

---
+++ src/main/java/com/crud/system/entity/Category.java
@@ -0,0 +1,32 @@
+package com.crud.system.entity;
+
+
+import jakarta.persistence.*;
+
+import java.util.Collection;
+
+@Entity
+@Table(name = "category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "category_sequence")
+    @SequenceGenerator(name = "category_sequence", sequenceName = "category_sequence", allocationSize = 1)
+    @Column(name = "category_id", nullable = false)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private Collection<Category> children;
+}
